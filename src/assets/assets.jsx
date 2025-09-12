4a0164f5e2095d5a55e3f0023ff707e6f8c7cc1a
AnmolPhad <120477856+AnmolPhad@users.noreply.github.com>
2025-09-12 16:46:18 +0530

    order

---
+++ src/assets/assets.jsx
@@ -0,0 +1,11 @@
+// src/assets/assets.jsx
+
+// --- DUMMY ADDRESS ---
+export const dummyAddress = {
+  fullName: "John Doe",
+  phoneNumber: "0123456789",
+  area: "123 Main Street",
+  city: "Metropolis",
+  state: "State",
+  pincode: 654321
+};
